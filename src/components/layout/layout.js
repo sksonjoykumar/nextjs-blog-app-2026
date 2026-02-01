51b25fa84baf0ef1887b9e6001d25b57866900d0
sksonjoykumar <sksonjoykumar111@gmail.com>
2026-02-01 18:14:28 +0600

    some changes

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,4 +1,3 @@
-import { getBlogPostsAction } from "@/src/actions/blog";
 import { verifyAuth } from "@/src/lib/auth";
 import { cookies } from "next/headers";
 import Footer from "../footer/Footer";
@@ -7,12 +6,11 @@ import Header from "../header/Header";
 export default async function CommonLayout({ children }) {
   const token = (await cookies()).get("token")?.value;
   const user = await verifyAuth(token);
-  const posts = await getBlogPostsAction();
- 
+
   return (
     <>
       <div>
-        {user && <Header posts={posts} />}
+        {user && <Header />}
         {children}
         {user && <Footer />}
       </div>
